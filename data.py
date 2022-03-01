addd2d42101ee0cf67da67e235c5cc59fe312204
Abheek Dhawan <abheekd@protonmail.com>
2022-03-01 09:05:50 -0600

    Use TKinter

---
+++ data.py
@@ -0,0 +1,118 @@
+'''
+A dictionary that stores the quiz questions, answers, and what they mean
+'''
+quizzes = {
+    "superhero": {
+        "questionsList": [
+            {
+                "question": "What do you like to do in your free time?",
+                "answers": [
+                    "Spend time by yourself",
+                    "Hang out with friends",
+                    "Play around with video games and tech",
+                    "Do athletics like gymnastics"
+                ]
+            },
+            {
+                "question": "What's your favorite sport?",
+                "answers": [
+                    "Motorsport",
+                    "Extreme sports",
+                    "Watersports",
+                    "Gymnastics"
+                ]
+            },
+            {
+                "question": "How rich are you?",
+                "answers": [
+                    "I-own-the-biggest-company-in-the-world rich",
+                    "I grew up on a farm",
+                    "I-own-the-second-biggest-company-in-the-world rich",
+                    "My dad changes work a lot"
+                ]
+            },
+            {
+                "question": "What's your dream job?",
+                "answers": [
+                    "Playboy martial artist",
+                    "Firefighter",
+                    "Playboy engineer",
+                    "Martial artist"
+                ]
+            },
+            {
+                "question": "If you could go anywhere, where would you go?",
+                "answers": [
+                    "Your mom's house",
+                    "Wherever people need help",
+                    "Ibiza",
+                    ""
+                ]
+            }
+        ],
+        "resultList": [
+            "Batman",
+            "Superman",
+            "Iron Man",
+            "Black Widow"
+        ]
+    },
+    "scientist": {      
+        "questionsList": [
+            {
+                "question": "What do you like to do in your free time?",
+                "answers": [
+                    "Spend time by yourself",
+                    "Hang out with friends",
+                    "Play around with video games and tech",
+                    "Do athletics like gymnastics"
+                ]
+            },
+            {
+                "question": "What's your favorite sport?",
+                "answers": [
+                    "Motorsport",
+                    "Extreme sports",
+                    "Watersports",
+                    "Gymnastics"
+                ]
+            },
+            {
+                "question": "How rich are you?",
+                "answers": [
+                    "I-own-the-biggest-company-in-the-world rich",
+                    "I grew up on a farm",
+                    "I-own-the-second-biggest-company-in-the-world rich",
+                    "My dad changes work a lot"
+                ]
+            },
+            {
+                "question": "What's your dream job?",
+                "answers": [
+                    "Playboy martial artist",
+                    "Firefighter",
+                    "Playboy engineer",
+                    "Martial artist"
+                ]
+            },
+            {
+                "question": "If you could go anywhere, where would you go?",
+                "answers": [
+                    "Your mom's house",
+                    "Wherever people need help",
+                    "Ibiza",
+                    ""
+                ]
+            }
+        ],
+        "resultList": [
+            "Albert Einstein",
+            "Niels Bohr",
+            "Marie Curie",
+            "Johannes Kepler"
+        ]
+    }
+}
+
+def getData():
+    return quizzes
--- quiz.py
+++ quiz.py
@@ -1,122 +1,30 @@
 from random import sample # Used to scramble arrays
 from statistics import mode # Used to calculate the most common answer
-import tkinter as tk
+import tkinter as tk # Used for UI
+from tkinter import messagebox # Used to display the message of the result
+from data import getData # Custom functino to get quiz data stored in another fle
+
 
 '''
-A dictionary that stores the quiz questions, answers, and what they mean
+Init vars
 '''
-quizzes = {
-    "superhero": {
-        "questionsList": [
-            {
-                "question": "What do you like to do in your free time?",
-                "answers": [
-                    "Spend time by yourself",
-                    "Hang out with friends",
-                    "Play around with video games and tech",
-                    "Do athletics like gymnastics"
-                ]
-            },
-            {
-                "question": "What's your favorite sport?",
-                "answers": [
-                    "Motorsport",
-                    "Extreme sports",
-                    "Watersports",
-                    "Gymnastics"
-                ]
-            },
-            {
-                "question": "How rich are you?",
-                "answers": [
-                    "I-own-the-biggest-company-in-the-world rich",
-                    "I grew up on a farm",
-                    "I-own-the-second-biggest-company-in-the-world rich",
-                    "My dad changes work a lot"
-                ]
-            },
-            {
-                "question": "What's your dream job?",
-                "answers": [
-                    "Playboy martial artist",
-                    "Firefighter",
-                    "Playboy engineer",
-                    "Martial artist"
-                ]
-            },
-            {
-                "question": "If you could go anywhere, where would you go?",
-                "answers": [
-                    "Your mom's house",
-                    "Wherever people need help",
-                    "Ibiza",
-                    ""
-                ]
-            }
-        ],
-        "resultList": [
-            "Batman",
-            "Superman",
-            "Iron Man",
-            "Black Widow"
-        ]
-    },
-    "scientist": {      
-        "questionsList": [
-            {
-                "question": "What do you like to do in your free time?",
-                "answers": [
-                    "Spend time by yourself",
-                    "Hang out with friends",
-                    "Play around with video games and tech",
-                    "Do athletics like gymnastics"
-                ]
-            },
-            {
-                "question": "What's your favorite sport?",
-                "answers": [
-                    "Motorsport",
-                    "Extreme sports",
-                    "Watersports",
-                    "Gymnastics"
-                ]
-            },
-            {
-                "question": "How rich are you?",
-                "answers": [
-                    "I-own-the-biggest-company-in-the-world rich",
-                    "I grew up on a farm",
-                    "I-own-the-second-biggest-company-in-the-world rich",
-                    "My dad changes work a lot"
-                ]
-            },
-            {
-                "question": "What's your dream job?",
-                "answers": [
-                    "Playboy martial artist",
-                    "Firefighter",
-                    "Playboy engineer",
-                    "Martial artist"
-                ]
-            },
-            {
-                "question": "If you could go anywhere, where would you go?",
-                "answers": [
-                    "Your mom's house",
-                    "Wherever people need help",
-                    "Ibiza",
-                    ""
-                ]
-            }
-        ],
-        "resultList": [
-            "Albert Einstein",
-            "Niels Bohr",
-            "Marie Curie",
-            "Johannes Kepler"
-        ]
-    }
-}
+results = [] # Empty results to be added to later
+
+root = tk.Tk() # Root window
+root.title("Quiz")
+quizTypeVar = tk.StringVar(value = "superhero") # Default to superhero
+questionVars = [] # Empty (to be added to later)
+
+quizzes = getData() # Get the data from the data.py file
+
+'''
+Frames to be raised over one another
+'''
+quizTypeFrame = tk.Frame(root) # Initial quiz type question
+questionsFrame = tk.Frame(root) # Questions from the quiz itsel
+
+for frame in (quizTypeFrame, questionsFrame):
+    frame.grid(row=0, column=0, sticky='news') # Add all the frames to the grid
 
 '''
 Scrambles the answers and results in the same order
@@ -137,45 +45,112 @@ def scramble_answers_results(answers, results):
     # Return a tuple with the two values
     return scrambledAnswers, scrambledResults
 
+
 '''
-Ask for which quiz they would like to take and ask
-questions based on that
+Adds to the results based on the answer choices
+and creates a message box to tell them what they
+got
 '''
-def ask_questions():
-    global quizzes
+def add_to_results():
+    # Get global variables to be used
+    global root
+    global results
 
-    answers = []
+    # Destroy the main window
+    root.destroy()
 
-    quizType = ""
-    # Loop through answers to which question they're asking
-    while True:
-        print("What quiz would you like to take?\n1. Superhero\n2. Scientist")
-        quizType = int(input())
-        if (quizType not in [1, 2]):
-            print("Bruv, that's not a type.")
-            continue
-        elif quizType == 1:
-            quizType = "superhero"
-        else:
-            quizType = "scientist"
-        break
-
+    # Add data from each string var to the results
+    for result in questionVars:
+        results.append(result.get())
     
-    questionsList = sample(quizzes[quizType]["questionsList"], len(quizzes[quizType]["questionsList"]))
+    messagebox.showinfo("Result", "You are " + mode(results) + "!") # Shows which superhero/scientist that they got
 
-    # Loop through each questions and scramble the questions and the answers,
-    # get input
-    for question in questionsList:
-        print("\n")
-        print(question['question']) # Print the current question
-        scrambledAnswers, scrambledResults = scramble_answers_results(question['answers'], quizzes[quizType]['resultList'])
-        for index, answer in enumerate(scrambledAnswers): # Print each answer
-            print(str(index + 1) + ". " + answer)
-        answer = int(input())
-        answer -= 1
-        answers.append(scrambledResults[answer]) # Add the person corresponding to the answer choice to an array
+'''
+Moves from asking which quiz type
+to asking questions for that quiz
+type
+'''
+def go_to_quiz_questions():
+    # Global vars
+    global questionsFrame
 
-    print("\n")
-    print("You are " + mode(answers) + "!") # Return the most frequent answer choice in the array for the result
+    # Raise the frame to be visible
+    questionsFrame.tkraise()
+    
+    # Get the questionsList and scramble it
+    questionsList = sample(quizzes[quizTypeVar.get()]["questionsList"], len(quizzes[quizTypeVar.get()]["questionsList"]))
 
-ask_questions()
+    # Loop through each questions and create
+    # 1. A label
+    # 2. Radio buttons for each answer
+    for index, question in enumerate(questionsList):
+        # Get scrambled answers and results
+        scrambledAnswers, scrambledResults = scramble_answers_results(question['answers'], quizzes[quizTypeVar.get()]['resultList'])
+        
+        # Create a temp string var to be later added to the array
+        tempStringVar = tk.StringVar(root, value = scrambledResults[0])
+        tk.Label(questionsFrame, 
+            text = question["question"],
+            justify = tk.LEFT,
+            padx = 20).grid(sticky = "w")
+
+        # Loop through each question to create a radio button
+        # and store the result value
+        for index, answer in enumerate(scrambledAnswers):
+            tk.Radiobutton(questionsFrame, 
+                text=answer,
+                padx = 20,
+                variable=tempStringVar,
+                value=scrambledResults[index]).grid(sticky = "w")
+
+        # Add the temporary string var to the array
+        questionVars.append(tempStringVar)
+
+    # Create a submit button and add it to the bottom of the grid
+    tk.Button(
+        questionsFrame,
+        text="Submit",
+        command=add_to_results
+    ).grid()
+
+'''
+Ask for the quiz type based on those defined in
+the quiz data
+'''
+def ask_quiz_type():
+    # Global vars
+    global quizzes
+    global quizTypeFrame
+
+    # Create a label on the top
+    tk.Label(quizTypeFrame, 
+        text = "What quiz would you like to take?",
+        justify = tk.LEFT,
+        padx = 20).grid()
+
+    # Add a new radio button for
+    # each quiz type
+    for quizType in quizzes:
+        tk.Radiobutton(quizTypeFrame, 
+            text=quizType.title(),
+            padx = 20,
+            value=quizType,
+            variable=quizTypeVar).grid(sticky = "w")
+    
+    # Create a button which, when pressed,
+    # will move to the 
+    tk.Button(quizTypeFrame,
+        text="Select",
+        command=go_to_quiz_questions
+    ).grid()
+
+    # Raise the newly created quiz type
+    # frame
+    quizTypeFrame.tkraise()
+
+'''
+Ask the quiz type to start the
+program
+'''
+ask_quiz_type()
+root.mainloop()
